bdf99c2a225a90723506773d567b07999efb0823
seb <sebbie777@gmail.com>
2017-01-17 17:22:09 -0700

    enable/disable functions for selection menu

--- js/app.js
+++ js/app.js
@@ -1,10 +1,37 @@
+"use strict";
+
+/*global $, jQuery, alert*/
+
 function testcar() {
 	$(".carousel").carousel(0);
 }
 
+
+function disableCitySelectMenu() {
+	$("#destinationCitySelectList").attr("disabled", true);
+}
+
+function enableCitySelectMenu() {
+	$("#destinationCitySelectList").attr("disabled", false);
+}
+
+function disableRegionSelectMenu() {
+	$("#destinationRegionSelectList").attr("disabled", true);
+}
+
+function enableRegionSelectMenu() {
+	$("#destinationRegionSelectList").attr("disabled", false);
+}
+
 $("#destinationCountrySelectList").change(function () {
     var selectedText = $(this).find("option:selected").text();
-    
-    $(".test").text(selectedText);
-	console.log(selectedText);
+    if (selectedText === "germany") {
+		disableCitySelectMenu();
+		disableRegionSelectMenu();
+		console.log(selectedText);
+	} else {
+		enableCitySelectMenu();
+		enableRegionSelectMenu();
+		console.log(selectedText);
+	}
 });
