60dfa7700c774cc095347db55927d6d36f23ab71
seb <sebbie777@gmail.com>
2017-01-19 19:37:36 -0700

    functions to disable enable fields, populate select drop downs and carousel image set

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 "use strict";
 
 /*global $, jQuery, alert*/
-
+/*
+// test func for setting img in carousel
 function testcar() {
-	$(".carousel").carousel(0);
+	//$(".carousel").carousel(0);
 }
+
+//test function for populating select list
+function test() {
+	var ddl = "#destinationCitySelectList";
+    $(ddl).append('<option value="' + "sss" + '">' + "aaa" + "</option>'");
+}
+*/
 
 
 function disableCitySelectMenu() {
@@ -23,9 +31,55 @@ function enableRegionSelectMenu() {
 	$("#destinationRegionSelectList").attr("disabled", false);
 }
 
+function enableHeight() {
+	$("#height").attr("disabled", false);
+}
+
+function enableLength() {
+	$("#length").attr("disabled", false);
+}
+
+function enableWidth() {
+	$("#width").attr("disabled", false);
+}
+
+function enableWeight() {
+	$("#weight").attr("disabled", false);
+}
+
+function disableHeight() {
+	$("#height").attr("disabled", true);
+}
+function disableLength() {
+	$("#length").attr("disabled", true);
+}
+function disableWidth() {
+	$("#width").attr("disabled", true);
+}
+function disableWeight() {
+	$("#weight").attr("disabled", true);
+}
+
+
+function testDisable(){
+	disableCitySelectMenu();
+	disableLength();
+	disableWeight() ;
+	 disableWidth();
+	 disableHeight();
+	 disableRegionSelectMenu();
+}
+
+
+
+
+
+
+
+
 $("#destinationCountrySelectList").change(function () {
     var selectedText = $(this).find("option:selected").text();
-    if (selectedText === "germany") {
+    if (selectedText !== "Russia") {
 		disableCitySelectMenu();
 		disableRegionSelectMenu();
 		console.log(selectedText);
@@ -35,3 +89,4 @@ $("#destinationCountrySelectList").change(function () {
 		console.log(selectedText);
 	}
 });
+
